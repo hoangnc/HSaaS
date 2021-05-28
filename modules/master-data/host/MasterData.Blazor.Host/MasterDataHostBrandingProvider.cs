61ab179a8b67e7a4b63f448a3a1ffe4d521c3e6f
hoangnc <diemhoang8488@gmail.com>
2021-05-28 08:50:55 +0700

    Delete unused files

--- modules/master-data/host/MasterData.Blazor.Host/MasterDataHostBrandingProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Ui.Branding;
-
-namespace MasterData.Blazor.Host
-{
-    public class MasterDataHostBrandingProvider : DefaultBrandingProvider
-    {
-        public override string AppName => "MasterData";
-    }
-}
